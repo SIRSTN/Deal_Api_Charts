746ce983b80d2abe4ba4a1c5acd300479fb650fc
SIRSTN <stephane.velen@gmail.com>
2024-03-06 15:55:25 +0100

    cloud commit

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import plotly
 import plotly.express as px
 import pandas as pd
 from pymongo import MongoClient
 import json
+from datetime import datetime
 from configparser import ConfigParser
 
 app = Flask(__name__)
@@ -17,8 +18,17 @@ config.read('config.ini')
 client = MongoClient("mongodb://localhost:27017/")
 db = client['Deal_Transactions']
 
-def get_total_valations():
-    result = db["Valuations"].aggregate([
+def parse_date(date_str):
+    """Parse a string date in 'YYYY-MM-DD' format into a datetime object, if provided."""
+    return datetime.strptime(date_str, '%Y-%m-%d') if date_str else None
+
+def get_total_valuations(from_date=None, to_date=None, keyword='Bitcoin'):
+    match_stage = {"$match": {"Keyword": keyword}}
+    if from_date and to_date:
+        match_stage["$match"].update({"Date": {"$gte": from_date, "$lte": to_date}})
+    
+    pipeline = [
+        match_stage,
         {
             "$group": {
                 "_id": "$Date",
@@ -31,19 +41,31 @@ def get_total_valations():
             }
         },
         {"$sort": {"_id": 1}}
-    ])
+    ]
+    
+    result = db["Valuations"].aggregate(pipeline)
     return pd.DataFrame(list(result))
 
-def get_deal_valations():
-    valuations = list(db["Valuations"].find({}, {'Date': 1, 'DealUID': 1, 'Volume': 1, 'Sold_Volume': 1, 'Init_Volume': 1, 'Price': 1}))
+def get_deal_valuations(from_date=None, to_date=None, keyword='Bitcoin'):
+    query = {"Keyword": keyword}
+    if from_date and to_date:
+        query.update({"Date": {"$gte": from_date, "$lte": to_date}})
+    
+    valuations = list(db["Valuations"].find(query))
     for valuation in valuations:
         valuation['Sold_Volume'] = float(valuation.get('Sold_Volume', 0)) * 1.05
         valuation['DealExpectedVolume'] = valuation['Volume'] + valuation['Sold_Volume'] - valuation.get('Init_Volume', 0)
     return pd.DataFrame(valuations)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    total_data = get_total_valations()
+    from_date_str = request.args.get('from_date', '2023-01-01')
+    to_date_str = request.args.get('to_date', '2023-12-31')
+    keyword = request.args.get('keyword', 'Bitcoin') 
+    from_date = parse_date(from_date_str)
+    to_date = parse_date(to_date_str)
+
+    total_data = get_total_valuations(from_date, to_date, keyword)
     total_data.rename(columns={
         '_id': 'Date',
         'TotalRealVolume': 'Total Real Volume',
@@ -53,8 +75,8 @@ def index():
         'TotalInitAmount': 'Total Init Amount',
         'TotalExpectedAmount': 'Total Expected Amount'
     }, inplace=True)
-    
-    deal_data = get_deal_valations()
+	
+    deal_data = get_deal_valuations(from_date, to_date, keyword)
     deal_data.rename(columns={
         'DealExpectedVolume': 'Deal Expected Volume'
     }, inplace=True)
@@ -72,7 +94,10 @@ def index():
     fig4 = px.line(total_data, x='Date', y=['Total Init Amount', 'Total Expected Amount'], title='<b>Total Expected Valuation</b><br><i>Init Amount AND real amount + expected amount</i>', labels={'value':'Expected', 'variable':'Type'})
     graphJSON4 = json.dumps(fig4, cls=plotly.utils.PlotlyJSONEncoder)
 
-    return render_template('chart.html', graphJSON1=graphJSON1, graphJSON2=graphJSON2, graphJSON3=graphJSON3, graphJSON4=graphJSON4)
+    return render_template('chart.html', 
+                           graphJSON1=graphJSON1, graphJSON2=graphJSON2, 
+                           graphJSON3=graphJSON3, graphJSON4=graphJSON4, 
+                           from_date=from_date_str, to_date=to_date_str, keyword=keyword)
 
 if __name__ == '__main__':
     app.run(debug=True)
